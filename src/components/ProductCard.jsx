63a11defea05c24636b3d42dcc166f292c9ab918
RamKashyap2050 <ramkashyap2050@gmail.com>
2026-01-22 15:08:28 -0500

    Updated Images and Data sync added new UI

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,16 +3,27 @@ import React from "react";
 export default function ProductCard({ item }) {
   return (
     <div className="card">
+      {item.image && (
+        <div className="cardImageWrap">
+          <img
+            src={item.image}
+            alt={item.name}
+            className="cardImage"
+            loading="lazy"
+          />
+        </div>
+      )}
+
       <div className="cardBody">
         <div className="cardHeaderRow">
           <h3 className="cardTitle">{item.name}</h3>
-          <div className="cardPrice">${Number(item.price).toFixed(2)}</div>
+          <span className="cardPrice">
+            ${Number(item.price).toFixed(2)}
+          </span>
         </div>
 
-        {item.description ? (
+        {item.description && (
           <p className="cardDesc">{item.description}</p>
-        ) : (
-          <p className="cardDesc cardDescMuted">No description.</p>
         )}
       </div>
     </div>
